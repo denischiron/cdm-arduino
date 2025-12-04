860e2d561bca4d8272e42c24228398cff3475a5d
Denis Chiron <chiron.denis@gmail.com>
2025-12-04 22:34:12 +0100

    ToggleRelay changes state, dont manages delay between states

--- arduino-electro-aimant-A8/sketch_pc_to_arduino_electro_aimant/sketch_pc_to_arduino_electro_aimant.ino
+++ arduino-electro-aimant-A8/sketch_pc_to_arduino_electro_aimant/sketch_pc_to_arduino_electro_aimant.ino
@@ -7,6 +7,7 @@
 
 int firstRelayDigitalID = 2;
 int arduinoLEDDigitalID = 13;
+int onOffDelay = 1000;
 
 
 void setup() {
@@ -30,20 +31,9 @@ void setup() {
 }
 
 // relayDigitalID : 1 à 5
-void toggleRelay(int relayDigitalID) {
-  
-  
-
-  // Met sous tension le relais (l'aimant est désactivé)
-  // Allume la LED
-  digitalWrite(relayDigitalID, HIGH);
-  digitalWrite(arduinoLEDDigitalID, HIGH);   
-  
-  delay (2000);
-  
-  // Annule tension du relais (l'aimant est réactivé)
-  digitalWrite(relayDigitalID, LOW);
-  digitalWrite(arduinoLEDDigitalID, LOW);
+void toggleRelay(int relayDigitalID, bool state) {  
+  digitalWrite(relayDigitalID, state);
+  digitalWrite(arduinoLEDDigitalID, state);   
 }
 
 void loop() {
@@ -56,26 +46,41 @@ void loop() {
     // Reçoit le caractère "A" : on désactive tous les aimants
     if (data == 'A') {
       
-      toggleRelay(firstRelayDigitalID);
-      toggleRelay(firstRelayDigitalID + 1);
-      toggleRelay(firstRelayDigitalID + 2);
-      toggleRelay(firstRelayDigitalID + 3);
-      toggleRelay(firstRelayDigitalID + 4);
+      // Met sous tension les relais (l'aimant est désactivé)
+      toggleRelay(firstRelayDigitalID, HIGH);
+      toggleRelay(firstRelayDigitalID + 1, HIGH);
+      toggleRelay(firstRelayDigitalID + 2, HIGH);
+      toggleRelay(firstRelayDigitalID + 3, HIGH);
+      toggleRelay(firstRelayDigitalID + 4, HIGH);
+
+      delay(onOffDelay);
+
+      // Annule tension du relais (l'aimant est réactivé)
+      toggleRelay(firstRelayDigitalID, LOW);
+      toggleRelay(firstRelayDigitalID + 1, LOW);
+      toggleRelay(firstRelayDigitalID + 2, LOW);
+      toggleRelay(firstRelayDigitalID + 3, LOW);
+      toggleRelay(firstRelayDigitalID + 4, LOW);
 
     } else {
       
       // Reçoit un ccaractère entre "1" et "5" : on en déduit le no entre 1 et 5
       int slotNo = data - '0';
+      int relayDigitalID = firstRelayDigitalID - 1 + slotNo;
 
       // 1 => firstRelayDigitalID
       // 2 => firstRelayDigitalID + 1
       // 3 => firstRelayDigitalID + 2
       // 4 => firstRelayDigitalID + 3
       // 5 => firstRelayDigitalID + 4
-      toggleRelay(firstRelayDigitalID - 1 + slotNo);
+      toggleRelay(relayDigitalID, HIGH);
+
+      delay(onOffDelay);
+
+      toggleRelay(relayDigitalID, LOW);
 
     }
   }
-  
+
   delay(100);          
 }
