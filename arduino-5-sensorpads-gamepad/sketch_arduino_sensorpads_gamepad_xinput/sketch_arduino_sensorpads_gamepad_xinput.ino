eaef18e9778357b262366d76b0d10968937aa033
Denis Chiron <chiron.denis@gmail.com>
2025-12-10 08:04:45 +0100

    Same Event on each sensopad : button_Y

--- arduino-5-sensorpads-gamepad/sketch_arduino_sensorpads_gamepad_xinput/sketch_arduino_sensorpads_gamepad_xinput.ino
+++ arduino-5-sensorpads-gamepad/sketch_arduino_sensorpads_gamepad_xinput/sketch_arduino_sensorpads_gamepad_xinput.ino
@@ -8,11 +8,18 @@
 #include "Sensor.h"
 
 Sensor sensorA1(A1, BUTTON_Y);
+Sensor sensorA2(A2, BUTTON_Y);
+Sensor sensorA3(A3, BUTTON_Y);
+Sensor sensorA4(A4, BUTTON_Y);
+Sensor sensorA5(A5, BUTTON_Y);
+
+/*
+Sensor sensorA1(A1, BUTTON_Y);
 Sensor sensorA2(A2, BUTTON_B);
 Sensor sensorA3(A3, BUTTON_A);
 Sensor sensorA4(A4, BUTTON_X);
 Sensor sensorA5(A5, DPAD_UP);
-
+*/
 
 void setup() {
   XInput.begin();
